4ecb37d87813a3abebdd041304496f819b7d3d78
medav <mdavies@iastate.edu>
2014-03-25 21:01:39 -0500

    More Edits

--- RDFTMinigame/physEngine/PHYSEngine.h
+++ RDFTMinigame/physEngine/PHYSEngine.h
@@ -15,8 +15,8 @@ public:
 
 	virtual void init();
 
-	virtual void AddEntity(ENTITY * ent) = 0;
-	virtual void Clear() = 0;
+	virtual void AddEntity(ENTITY * ent);
+	virtual void Clear();
 
 	virtual void Think();
 
